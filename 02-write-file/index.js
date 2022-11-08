19598751940dbf72e4661782de1b8b911158dc8d
Семененко Надежда <tn@impsa.ru>
2022-11-08 10:24:23 +0300

    fix: correct task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,27 +1,25 @@
-//Импорт всех требуемых модулей
 const fs = require('fs');
 const path = require('path');
 const { stdin, stdout } = process;
+const filePath = path.join(__dirname, 'destination.txt');
 
-// Создание потока записи в текстовый файл???
-fs.open(path.join(__dirname, 'destination.txt'), 'w', () => {});
-
-// Вывод в консоль приветственного сообщения
-stdout.write('Добрейшего вечерочка! Введите текст в консоль.\n');
-
-// Ожидание ввода текста пользователем, с проверкой ввода на наличие ключевого слова exit
-stdin.on('data', data => {
-    if (data.toString().trim() == 'exit') {
-        process.exit();
-    }
-    //Запись текста в файл
-    fs.appendFile(path.join(__dirname, 'destination.txt'), data.toString(), () => {});
-    //Ожидание дальнейшего ввода
-    stdout.write('Снова введите текст в консоль.\n');
-})
-
-//Реализация прощального сообщения при остановке процесса
-process.on('exit' || 'SIGINT', () => {
-    stdout.write('ВсегО ХорошегО!\n');
+const toExit = () => {
+    stdout.write('Bye bye!\n');
     process.exit();
+};
+
+fs.open(filePath, 'w', () => {});
+
+stdout.write('Hi! Enter your text\n');
+
+process.on('SIGINT', toExit)
+
+stdin.on('data', chunk => {
+    if (chunk.toString().trim() == 'exit') {
+        toExit();
+    }
+    fs.appendFile(filePath, chunk.toString(), () => {});
+    stdout.write('Enter your text again\n');
 })
+
+

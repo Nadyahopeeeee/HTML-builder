7e4c252c953cb8b184e6be4eadb3b274da1e5c71
Семененко Надежда <tn@impsa.ru>
2022-11-08 14:33:43 +0300

    feat: task 4 done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
 
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+
+const inputPath = `${__dirname}/files/`;
+const outputPath = `${__dirname}/files-copy/`;
+
+const copyFile = () => {
+    fs.readdir(inputPath, (error, files) => {
+    files.forEach(elem => {
+        fs.copyFile(
+            inputPath + elem,
+            outputPath + elem,
+            (error) => {if (error) throw error}
+        );
+    });
+        stdout.write('Yahoo! All files copied.\n');
+    }
+)};
+
+const createDir = () => {
+    fs.mkdir(path.join(__dirname, 'files-copy'), (error) => {
+        stdout.write('New directory already created.\n');
+    });
+}
+
+fs.rm(outputPath, { recursive: true }, error => {
+    copyFile();
+    createDir();
+})

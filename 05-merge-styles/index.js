cda2548de65ef1c91efd31064d6356c1ec5b53dc
Семененко Надежда <tn@impsa.ru>
2022-11-09 16:03:23 +0300

    fix: correct task 5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -27,32 +27,3 @@ fs.readdir(input, (error, files) => {
         }
     });
 });
-
-// const fs = require("fs");
-// const path = require("path");
-
-// const inputPath = path.join(__dirname, "styles");
-// const outputPath = path.join(__dirname, "project-dist");
-
-// fs.writeFile(path.join(outputPath, "bundle.css"), "", (err) => {
-//   if(err) throw err;
-// } )
-
-// fs.readdir(inputPath, (err, files) => {
-//   files.forEach(file => {
-//     let filePath = path.join(inputPath, file);
-//     if(path.extname(filePath) == ".css") {
-
-//         let fileContents = "";
-//         fs.readFile(filePath, (err, data) => {
-//           if(err) throw err;
-//           fileContents = data.toString();
-//           fs.appendFile(path.join(outputPath, "bundle.css"), fileContents, (err) => {
-//             if(err) throw err;
-//             console.log(`${file} is appended to bundle.css`)
-//           })
-//         })
-//     }
-    
-//   })
-// })
